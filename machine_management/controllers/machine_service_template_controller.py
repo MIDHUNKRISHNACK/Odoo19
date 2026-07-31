d9a8167646c54baa5a9a95fc518d1fa1c4c6086c
MIDHUNKRISHNA C K <mithunkrishna804@gmail.com>
2026-07-31 17:24:27 +0530

    website2

--- machine_management/controllers/machine_service_template_controller.py
+++ machine_management/controllers/machine_service_template_controller.py
@@ -2,11 +2,42 @@ from odoo import http
 from odoo.http import request
 
 class MachineServiceTemplateController(http.Controller):
+    @http.route('/newuser-odoo', type='http', auth='public', website=True)
+    def create_new_customer(self, **post):
+        return request.render('machine_management.new_user_registration')
+
+
+    @http.route('/customer-create', type='http', auth='public', website=True,methods=['POST'],csrf=True)
+    def create_new_user(self, **post):
+        if post.get('user_type')=='person':
+            request.env['res.partner'].sudo().create({
+                'company_type':'person',
+                'name': post.get('name'),
+                'phone': post.get('phone_number'),
+                'email': post.get('email'),
+                'street': post.get('address')
+                          })
+        else:
+            request.env['res.partner'].sudo().create({
+                'company_type': 'company',
+                'name': post.get('name'),
+                'phone': post.get('phone_number'),
+                'email': post.get('email'),
+                'street': post.get('address')
+            })
+
+        return request.render('website.contactus_thanks')
+
+    @http.route(['/thanks'], type='http', auth="public", methods=['GET'], website=True)
+    def service_list(self, **post):
+        return request.redirect('/contactus-thank-you')
+
     @http.route('/service-odoo', type='http', auth='public', website=True)
     def machine_service(self, **kwargs):
         print(self)
         customer_ids=request.env['res.partner'].sudo().search([])
         machine_ids=request.env['machine.machine'].sudo().search([])
+        machine_type_ids=request.env['machine.machine.types'].sudo().search([])
         user_name = request.env.user.name if request.env.user.id else 'Guest'
         print(user_name)
 
@@ -14,13 +45,18 @@ class MachineServiceTemplateController(http.Controller):
             'user_name': user_name,
             'customer_ids': customer_ids,
             'machine_ids': machine_ids,
+            'machine_type_ids':machine_type_ids
         })
 
     @http.route(['/service-create'], type='http', auth="public", methods=['POST'], website=True, csrf=True)
     def create_customer(self, **post):
+        print("hiihiii")
         print("self =",self)
         print("request =",request)
         print("post =",post)
+        print("customer =",post.get('customer_id'))
+        print("machine_name =",post.get('machine_name'))
+        print("purchase_date =",post.get('purchase_date'))
         # cus_name=request.env['res.partner'].sudo().create({
         #     'name': post.get('cus_name'),
         #
@@ -37,4 +73,7 @@ class MachineServiceTemplateController(http.Controller):
             'date_of_service': post.get('purchase_date'),
 
         })
-        return request.render('machine_management.service_register_success_template')
+        # return request.render('website.contactus_thanks')
+
+
+
