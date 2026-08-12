fb58813deaef4894aada98680c69094a62959ca9
MIDHUNKRISHNA C K <mithunkrishna804@gmail.com>
2026-08-12 17:50:58 +0530

    website10

--- pos_rating/model/product_template.py
+++ pos_rating/model/product_template.py
@@ -2,7 +2,7 @@ from odoo import fields, models, api
 class ProductProduct(models.Model):
     _inherit = "product.template"
 
-    product_rating=fields.Selection([('one','1'),('two','2'),('three','3'),('four','4'),('five','5')])
+    product_rating=fields.Selection([('one','1'),('two','2'),('three','3'),('four','4'),('five','5'),('six','6')])
 
     @api.model
     def _load_pos_data_fields(self, config):
@@ -15,3 +15,4 @@ class ProductProduct(models.Model):
         print("new result", result)
         print("len of result", len(result))
         return result
+
--- pos_rating/static/src/js/pos_product_card.js
+++ pos_rating/static/src/js/pos_product_card.js
@@ -1,12 +1,20 @@
-import { patch } from "@web/core/utils/patch";
-import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
-import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+/** @odoo-module */
 import { _t } from "@web/core/l10n/translation";
-patch(ProductScreen.prototype, {
-   clickCustomAction() {
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { patch } from "@web/core/utils/patch";
+import { ActionpadWidget } from "@point_of_sale/app/screens/product_screen/action_pad/action_pad";
+
+console.log("this",this);
+patch(ActionpadWidget.prototype, {
+       clickNew() {
+           const current_order=this.currentOrder.lines
+           console.log("current_order",current_order)
+
+
        this.dialog.add(AlertDialog, {
-           title: _t("Custom Action"),
-           body: _t("This popup is triggered from a custom Control Button."),
+           title:_t("Low Rating Products"),
+           body: _t("You have Choosed an Low Rating Product in Order "),
+
        });
    },
 });
